21b94e8f8e6fc0e16082c26f0b211e2bd0a72915
root <root@iZ8vbfl2ade2rd6gefxupyZ.(none)>
2018-10-14 18:59:46 +0800

    fix

--- config/database.php
+++ config/database.php
@@ -13,15 +13,15 @@ return [
 // 数据库类型
 'type'            => 'mysql',
 // 服务器地址
-'hostname'        => 'localhost',
+'hostname'        => '127.0.0.1',
 // 数据库名
-'database'        => 'rhaphpv3',
+'database'        => 'restaurant',
 // 用户名
-'username'        => 'root',
+'username'        => 'test',
 // 密码
-'password'        => 'root',
+'password'        => '123456',
 // 端口
-'hostport'        => '3306',
+'hostport'        => '4051',
 // 连接dsn
 'dsn'             => '',
 // 数据库连接参数
@@ -29,7 +29,7 @@ return [
 // 数据库编码默认采用utf8mb4
 'charset'         => 'utf8mb4',
 // 数据库表前缀
-'prefix'          => 'rh_',
+'prefix'          => 're_',
 // 数据库调试模式
 'debug'           => true,
 // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
